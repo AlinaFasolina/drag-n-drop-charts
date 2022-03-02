6d0d2b0cf979dddab2ce2bffad16ae8b8e1991bb
kseniabugrim <ksenia.bugrim.work@gmail.com>
2022-03-02 15:08:58 +0200

    fix toolbar

---
+++ src/components/charts/MyStockChart.js
@@ -0,0 +1,20 @@
+export const SC1 = {
+  rangeSelector: {
+    selected: 1
+  },
+
+  title: {
+    text: 'AAPL Stock Price'
+  },
+
+  series: [{
+    name: 'AAPL',
+    data: [1,2,3],
+    tooltip: {
+        valueDecimals: 2
+    }
+  }]
+}
+
+
+
